d6bfd95cf101e889f63216e85dadf9ee5c73826f
Ronak Shah <ronakshah678@gmail.com>
2018-07-28 15:38:28 -0400

    client settings page update
    - profile page reverted back to read only
    - update profile data on page load
    - add bio and fav prods. field in Settings -> Info to edit

--- src/pages/client-profile/client-profile.ts
+++ src/pages/client-profile/client-profile.ts
@@ -11,11 +11,14 @@ import {ClientSettingsPage} from "../client-settings/client-settings";
 export class ClientProfilePage {
   rate= 3.5;
   profile: string = "ABOUT";
-  editMode: boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams, private utils: UtilsProvider) {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ClientProfilePage');
+  }
+
+  ngOnInit(){
+    this.utils.getProfile().then(data => console.log("Profile fetched."));
   }
 
   onModelChange(data){
@@ -24,19 +27,5 @@ export class ClientProfilePage {
   openSettings(){
     this.utils.setPage(ClientSettingsPage);
     this.utils.notifyOther('data');
-  }
-
-  editToggle(){
-    this.editMode = true;
-  }
-
-  saveProfile(){
-    let ref = this;
-    this.utils.profile.fav = this.utils.profile.fav.replace(/,/g," . ");
-    // this.utils.editClientSettings(this.utils.profile).then(function (data) {
-    //   console.log(data);
-    //   ref.editMode = false;
-    // });
-    ref.editMode = false;
   }
 }
